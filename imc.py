e744718252967e6b4a396fa86f5b90cb9beceb30
Grier Phillips <GrierPhillips@gmail.com>
2017-07-25 14:20:43 -0600

    Start imc.py
    imc.py will contain the implementation of Inductive Matrix Completion.

---
+++ imc.py
@@ -0,0 +1,10 @@
+"""
+Implementation of Inductive Matric Completion.
+
+Inductive Matrix Completion (IMC) is an algorithm for recommender systems with
+side-information of users and items. The IMC formulation incorporates features
+associated with rows (users) and columns (items) in matrix completion, so that
+it enables predictions for users or items that were not seen during training,
+and for which only features are known but no dyadic information (such as
+ratings or linkages).
+"""
