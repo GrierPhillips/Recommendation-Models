4fe181175a9786ce9fcb1e295b865686ffd60617
Grier Phillips <GrierPhillips@gmail.com>
2017-08-16 12:25:48 -0600

    Step back on tests
    Step back on tests and implement only test_fv for now. Rewrite the setup method
    to setup three instances of IMC for testing different values of l1_ratio.

--- tests/test_imc.py
+++ tests/test_imc.py
@@ -24,21 +24,28 @@ class IMCTest(unittest.TestCase):
 
     def setUp(self):
         """Set up the IMC class before each test."""
-        self.imc = IMC(rank=10, max_iter=20, lambda_=0.01)
+        self.imc0 = IMC(max_iter=20, alpha=0.01, l1_ratio=0)
+        self.imc1 = IMC(max_iter=20, alpha=0.01, l1_ratio=1)
+        self.imc0_5 = IMC(max_iter=20, alpha=0.01, l1_ratio=0.5)
         item_att_feats = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
         user_atts = np.array([[1, 2, 3], [4, 5, 6]])
         item_atts = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
         user_att_feats = np.array([[1, 2, 3], [4, 5, 6]])
         ratings = np.array([[300, 6000], [9000, 12000]])
-        self.args = (
-            user_att_feats,
-            user_atts,
-            item_atts,
-            ratings,
-            self.imc.lambda_,
-            item_att_feats.shape
-        )
-        self.in_array = item_att_feats.flatten()
+        self.args_v = {
+            key: (
+                user_att_feats, user_atts, item_atts, ratings, 0.01, l1,
+                item_att_feats.shape
+            ) for (key, l1) in zip([0, 1, 0.5], [0, 1, 0.5])
+        }
+        self.args_u = {
+            key: (
+                item_att_feats, user_atts, item_atts, ratings, 0.01, l1,
+                user_att_feats.shape
+            ) for (key, l1) in zip([0, 1, 0.5], [0, 1, 0.5])
+        }
+        self.input_v = item_att_feats.flatten()
+        self.input_u = user_att_feats.flatten()
 
     def tearDown(self):
         """Teardown the IMC class after each test."""
@@ -54,77 +61,47 @@ class IMCTest(unittest.TestCase):
 
     def test_fv(self):
         """The _fv function should return the regularized sum squared error."""
-        expected = 26127448.952889957
-        actual = self.imc._fv(self.in_array, *self.args)
+        expected_0 = 13063724.476444978
+        actual_0 = self.imc0._fv(self.input_v, *self.args_v[0])
         self.assertEqual(
-            expected,
-            actual,
-            msg='Expected {}, but found {}.'.format(expected, actual)
+            expected_0,
+            actual_0,
+            msg='Expected {}, but found {}.'.format(expected_0, actual_0)
+        )
+        expected_1 = 13063724.84
+        actual_1 = self.imc1._fv(self.input_v, *self.args_v[1])
+        self.assertEqual(
+            expected_1,
+            actual_1,
+            msg='Expected {}, but found {}.'.format(expected_1, actual_1)
+        )
+        expected_2 = 13063724.658222489
+        actual_2 = self.imc0_5._fv(self.input_v, *self.args_v[0.5])
+        self.assertEqual(
+            expected_2,
+            actual_2,
+            msg='Expected {}, but found {}.'.format(expected_2, actual_2)
         )
 
     def test_fv_prime(self):
         """The _fv_prime function should return the gradient of the regularized sum squared error with respect to V."""  # noqa
-        expected = np.array(
-            [568680.01, 641008.02, 713336.03, 785664.04, 1359780.05,
-             1528912.06, 1698044.07, 1867176.08]
-        )
-        actual = self.imc._fv_prime(self.in_array, *self.args)
-        np.testing.assert_array_equal(
-            expected,
-            actual,
-            err_msg='Expected {}, but found {}.'.format(expected, actual)
-        )
+        pass
 
     def test_fv_hess(self):
         """The _fv_hess function should return the hessian of the regularized sum squared error with respect to V."""  # noqa
-        expected = np.array(
-            [2622400., 3306176.01, 3989952.02, 4673728.03, 6261280.04,
-             7893872.05, 9526464.06, 11159056.07]
-        )
-        hess_p = np.arange(8)
-        actual = self.imc._fv_hess(self.in_array, hess_p, *self.args)
-        np.testing.assert_array_equal(
-            expected,
-            actual,
-            err_msg='Expected {}, but found {}.'.format(expected, actual)
-        )
+        pass
 
     def test_fu(self):
         """The _fu function should return the regularized sum squared error."""
-        expected = 26127448.952889957
-        actual = self.imc._fu(self.in_array, *self.args)
-        self.assertEqual(
-            expected,
-            actual,
-            msg='Expected {}, but found {}.'.format(expected, actual)
-        )
+        pass
 
     def test_fu_prime(self):
         """The _fu_prime function should return the gradient of the regularized sum squared error with respect to U."""  # noqa
-        expected = np.array(
-            [809800.01, 1094120.02, 1378440.03, 2050600.04, 2758664.05,
-             3466728.06]
-        )
-        actual = self.imc._fu_prime(self.in_array, *self.args)
-        np.testing.assert_array_equal(
-            expected,
-            actual,
-            err_msg='Expected {}, but found {}.'.format(expected, actual)
-        )
+        pass
 
     def test_fu_hess(self):
         """The _fu_hess function should return the hessian of the regularized sum squared error with respect to U."""  # noqa
-        expected = np.array(
-            [4353520., 5755160.01, 7156800.02, 10723504.03, 14175992.04,
-             17628480.05]
-        )
-        hess_p = np.arange(6)
-        actual = self.imc._fu_hess(self.in_array, hess_p, *self.args)
-        np.testing.assert_array_equal(
-            expected,
-            actual,
-            err_msg='Expected {}, but found {}.'.format(expected, actual)
-        )
+        pass
 
     def test_transform(self):
         """The transform method should return the W matrix from the fitted model."""  # noqa
