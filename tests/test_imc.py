c82daa9cc708607ddf8376ed404e50ea7ae2fadf
Grier Phillips <GrierPhillips@gmail.com>
2017-08-16 12:47:09 -0600

    Implement all fv gradient tests

--- tests/test_imc.py
+++ tests/test_imc.py
@@ -85,11 +85,70 @@ class IMCTest(unittest.TestCase):
 
     def test_fv_prime(self):
         """The _fv_prime function should return the gradient of the regularized sum squared error with respect to V."""  # noqa
-        pass
+        expected_0 = np.array(
+            [568680.01, 641008.02, 713336.03, 785664.04, 1359780.05,
+             1528912.06, 1698044.07, 1867176.08]
+        )
+        actual_0 = self.imc0._fv_prime(self.input_v, *self.args_v[0])
+        np.testing.assert_array_equal(
+            expected_0,
+            actual_0,
+            err_msg='Expected {}, but found {}.'.format(expected_0, actual_0)
+        )
+        expected_1 = np.array(
+            [568680.005, 641008.005, 713336.005, 785664.005, 1359780.005,
+             1528912.005, 1698044.005, 1867176.005]
+        )
+        actual_1 = self.imc1._fv_prime(self.input_v, *self.args_v[1])
+        np.testing.assert_array_equal(
+            expected_1,
+            actual_1,
+            err_msg='Expected {}, but found {}.'.format(expected_1, actual_1)
+        )
+        expected_2 = np.array(
+            [568680.0075, 641008.0125, 713336.0175, 785664.0225, 1359780.0275,
+             1528912.0325, 1698044.0375, 1867176.0425]
+        )
+        actual_2 = self.imc0_5._fv_prime(self.input_v, *self.args_v[0.5])
+        np.testing.assert_allclose(
+            expected_2,
+            actual_2,
+            err_msg='Expected {}, but found {}.'.format(expected_2, actual_2)
+        )
 
     def test_fv_hess(self):
         """The _fv_hess function should return the hessian of the regularized sum squared error with respect to V."""  # noqa
-        pass
+        expected_0 = np.array(
+            [2622400., 3306176.01, 3989952.02, 4673728.03, 6261280.04,
+             7893872.05, 9526464.06, 11159056.07]
+        )
+        hess_p = np.arange(8)
+        actual_0 = self.imc0._fv_hess(self.input_v, hess_p, *self.args_v[0])
+        np.testing.assert_array_equal(
+            expected_0,
+            actual_0,
+            err_msg='Expected {}, but found {}.'.format(expected_0, actual_0)
+        )
+        expected_1 = np.array(
+            [2622400., 3306176., 3989952., 4673728., 6261280.,
+             7893872., 9526464., 11159056.]
+        )
+        actual_1 = self.imc1._fv_hess(self.input_v, hess_p, *self.args_v[1])
+        np.testing.assert_array_equal(
+            expected_1,
+            actual_1,
+            err_msg='Expected {}, but found {}.'.format(expected_1, actual_1)
+        )
+        expected_2 = np.array(
+            [2622400., 3306176.005, 3989952.01, 4673728.015, 6261280.02,
+             7893872.025, 9526464.03, 11159056.035]
+        )
+        actual_2 = self.imc0_5._fv_hess(self.input_v, hess_p, *self.args_v[0.5])
+        np.testing.assert_array_equal(
+            expected_2,
+            actual_2,
+            err_msg='Expected {}, but found {}.'.format(expected_2, actual_2)
+        )
 
     def test_fu(self):
         """The _fu function should return the regularized sum squared error."""
