0a1f19fa66a872bc3ca8c6a1c9b62eb7f69e12c1
Grier Phillips <GrierPhillips@gmail.com>
2017-08-16 13:13:54 -0600

    Implement gradient function for use with scipy.optimize

--- src/imc.py
+++ src/imc.py
@@ -9,6 +9,9 @@ and for which only features are known but no dyadic information (such as
 ratings or linkages).
 """
 
+import numpy as np
+from numpy.linalg import norm
+
 
 class IMC(object):
     """Implementation of Inductive Matrix Completion.
@@ -48,7 +51,13 @@ class IMC(object):
     """
 
     def __init__(self, n_components=None, max_iter=30, alpha=0.1, l1_ratio=0):
-        pass
+        """Create instance of IMC with given parameters.
+
+        """
+        self.n_components = n_components
+        self.max_iter = max_iter
+        self.alpha = alpha
+        self.l1_ratio = l1_ratio
 
     def fit_transform(self, ratings, X, y):
         pass
@@ -58,26 +67,133 @@ class IMC(object):
 
     @staticmethod
     def _fv(V, *args):
-        pass
+        """Return the regularized sum squared error for a given V.
+
+        Parameters
+        ----------
+        V: array-like, shape (n_samples, n_feature)
+            Component to minimize the cost function against.
+
+        Returns
+        -------
+        gv: The sum squared error for the given V.
+
+        """
+        U, X, Y, R, lam, l1_ratio, shape = args
+        V = V.reshape(shape)
+        gv = np.sum(
+            0.5 * np.asarray(R - X.dot(U.T).dot(V).dot(Y.T)) ** 2 + 0.5 * lam *
+            (1 - l1_ratio) * (norm(U) + norm(V)) + lam * l1_ratio *
+            (norm(U, 1) + norm(V, 1))
+        )
+        return gv
 
     @staticmethod
     def _fv_prime(V, *args):
-        pass
+        """Return the gradient of the regularized SSE for a given V.
+
+        Parameters
+        ----------
+        V: array-like, shape (n_samples, n_features)
+            Component to minimize the cost function against.
+
+        Returns
+        -------
+        gv: The gradient of the regularized sum squared error for the given V.
+
+        """
+        U, X, Y, R, lam, l1_ratio, shape = args
+        V = V.reshape(shape)
+        gv = U.dot(X.T).dot(X).dot(U.T).dot(V).dot(Y.T).dot(Y) -\
+            R.T.dot(X).dot(U.T).T.dot(Y) + (1 - l1_ratio) * lam * V + 0.5 *\
+            l1_ratio * lam
+        return gv.flatten()
 
     @staticmethod
     def _fv_hess(V, p, *args):
-        pass
+        """Return the hessian of the regularized SSE for a given V.
+
+        Parameters
+        ----------
+        V: array-like, shape (n_samples, n_features)
+            Component to minimize the cost function against.
+
+        Returns
+        -------
+        gv: The hessian of the regularized sum squared error for the given V.
+
+        """
+        U, X, Y, R, lam, l1_ratio, shape = args
+        S = p.reshape(shape)
+        gv = U.dot(X.T).dot(X).dot(U.T).dot(S).dot(Y.T).dot(Y) +\
+            (1 - l1_ratio) * lam * S
+        return gv.flatten()
 
     @staticmethod
     def _fu(U, *args):
-        pass
+        """Return the hessian of the regularized SSE for a given U.
+
+        Parameters
+        ----------
+        U: array-like, shape (n_samples, n_features)
+            Component to minimize the cost function against.
+
+        Returns
+        -------
+        gu: The sum squared error for the given U.
+
+        """
+        V, X, Y, R, lam, l1_ratio, shape = args
+        U = U.reshape(shape)
+        gu = np.sum(
+            0.5 * np.asarray(R - X.dot(U.T).dot(V).dot(Y.T)) ** 2 + 0.5 * lam *
+            (1 - l1_ratio) * (norm(U) + norm(V)) + lam * l1_ratio *
+            (norm(U, 1) + norm(V, 1))
+        )
+        return gu
 
     @staticmethod
     def _fu_prime(U, *args):
-        pass
+        """Return the hessian of the regularized SSE for a given U.
+
+        Parameters
+        ----------
+        U: array-like, shape (n_samples, n_features)
+            Component to minimize the cost function against.
+
+        Returns
+        -------
+        gu: The gradient of the regularized sum squared error for the given U.
+
+        """
+        V, X, Y, R, lam, l1_ratio, shape = args
+        U = U.reshape(shape)
+        gu = V.dot(Y.T).dot(Y).dot(V.T).dot(U).dot(X.T).dot(X) -\
+            R.dot(Y).dot(V.T).T.dot(X) + lam * (1 - l1_ratio) * U +\
+            l1_ratio * lam
+        return gu.flatten()
 
     @staticmethod
     def _fu_hess(U, p, *args):
+        """Return the hessian of the regularized SSE for a given U.
+
+        Parameters
+        ----------
+        U: array-like, shape (n_samples, n_features)
+            Component to minimize the cost function against.
+
+        Returns
+        -------
+        gu: The hessian of the regularized sum squared error for the given U.
+
+        """
+        V, X, Y, R, lam, l1_ratio, shape = args
+        S = p.reshape(shape)
+        gu = V.dot(Y.T).dot(Y).dot(V.T).dot(S).dot(X.T).dot(X) +\
+            (1 - l1_ratio) * lam * S
+        return gu.flatten()
+
+    def transform(self):
         pass
 
     def predict_one(self):
