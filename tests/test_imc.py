8b2da12d76ab866f7921e1ffdf2e0a7a48c9de82
Grier Phillips <GrierPhillips@gmail.com>
2017-08-16 16:27:40 -0600

    Update tests to reflect moving gradient functions outside of IMC class

--- tests/test_imc.py
+++ tests/test_imc.py
@@ -6,7 +6,7 @@ import unittest
 
 import numpy as np
 
-from src.imc import IMC
+from src.imc import IMC, _fh, _fh_hess, _fh_prime, _fw, _fw_hess, _fw_prime
 
 
 class IMCTest(unittest.TestCase):
@@ -66,8 +66,8 @@ class IMCTest(unittest.TestCase):
     def test_fit_transform(self):
         """The fit_transform method should iterate through the algorithm until the fit condition is met, finally returning the W matrix."""  # noqa
         expected_0 = np.array(
-            [[-0.83361051, -0.15117396, 0.53126259],
-             [-0.37205758, -0.55720711, -0.74235665],
+            [[-0.833617, -0.15118367, 0.53124965],
+             [-0.37204305, -0.55720448, -0.74236591],
              [0.40824829, -0.81649658, 0.40824829]]
         )
         actual_0 = self\
@@ -81,21 +81,21 @@ class IMCTest(unittest.TestCase):
     def test_fh(self):
         """The _fh function should return the regularized sum squared error."""
         expected_0 = 13063724.476444978
-        actual_0 = self.imc0._fh(self.input_h, *self.args_h[0])
+        actual_0 = _fh(self.input_h, *self.args_h[0])
         self.assertEqual(
             expected_0,
             actual_0,
             msg='Expected {}, but found {}.'.format(expected_0, actual_0)
         )
         expected_1 = 13063724.84
-        actual_1 = self.imc1._fh(self.input_h, *self.args_h[1])
+        actual_1 = _fh(self.input_h, *self.args_h[1])
         self.assertEqual(
             expected_1,
             actual_1,
             msg='Expected {}, but found {}.'.format(expected_1, actual_1)
         )
         expected_2 = 13063724.658222489
-        actual_2 = self.imc0_5._fh(self.input_h, *self.args_h[0.5])
+        actual_2 = _fh(self.input_h, *self.args_h[0.5])
         self.assertEqual(
             expected_2,
             actual_2,
@@ -108,7 +108,7 @@ class IMCTest(unittest.TestCase):
             [568680.01, 641008.02, 713336.03, 785664.04, 1359780.05,
              1528912.06, 1698044.07, 1867176.08]
         )
-        actual_0 = self.imc0._fh_prime(self.input_h, *self.args_h[0])
+        actual_0 = _fh_prime(self.input_h, *self.args_h[0])
         np.testing.assert_array_equal(
             expected_0,
             actual_0,
@@ -118,7 +118,7 @@ class IMCTest(unittest.TestCase):
             [568680.005, 641008.005, 713336.005, 785664.005, 1359780.005,
              1528912.005, 1698044.005, 1867176.005]
         )
-        actual_1 = self.imc1._fh_prime(self.input_h, *self.args_h[1])
+        actual_1 = _fh_prime(self.input_h, *self.args_h[1])
         np.testing.assert_array_equal(
             expected_1,
             actual_1,
@@ -128,7 +128,7 @@ class IMCTest(unittest.TestCase):
             [568680.0075, 641008.0125, 713336.0175, 785664.0225, 1359780.0275,
              1528912.0325, 1698044.0375, 1867176.0425]
         )
-        actual_2 = self.imc0_5._fh_prime(self.input_h, *self.args_h[0.5])
+        actual_2 = _fh_prime(self.input_h, *self.args_h[0.5])
         np.testing.assert_allclose(
             expected_2,
             actual_2,
@@ -142,7 +142,7 @@ class IMCTest(unittest.TestCase):
              7893872.05, 9526464.06, 11159056.07]
         )
         hess_p = np.arange(8)
-        actual_0 = self.imc0._fh_hess(self.input_h, hess_p, *self.args_h[0])
+        actual_0 = _fh_hess(self.input_h, hess_p, *self.args_h[0])
         np.testing.assert_array_equal(
             expected_0,
             actual_0,
@@ -152,7 +152,7 @@ class IMCTest(unittest.TestCase):
             [2622400., 3306176., 3989952., 4673728., 6261280.,
              7893872., 9526464., 11159056.]
         )
-        actual_1 = self.imc1._fh_hess(self.input_h, hess_p, *self.args_h[1])
+        actual_1 = _fh_hess(self.input_h, hess_p, *self.args_h[1])
         np.testing.assert_array_equal(
             expected_1,
             actual_1,
@@ -162,8 +162,7 @@ class IMCTest(unittest.TestCase):
             [2622400., 3306176.005, 3989952.01, 4673728.015, 6261280.02,
              7893872.025, 9526464.03, 11159056.035]
         )
-        actual_2 = self.\
-            imc0_5._fh_hess(self.input_h, hess_p, *self.args_h[0.5])
+        actual_2 = _fh_hess(self.input_h, hess_p, *self.args_h[0.5])
         np.testing.assert_array_equal(
             expected_2,
             actual_2,
@@ -173,21 +172,21 @@ class IMCTest(unittest.TestCase):
     def test_fw(self):
         """The _fw function should return the regularized sum squared error."""
         expected_0 = 13063724.476444978
-        actual_0 = self.imc0._fw(self.input_w, *self.args_w[0])
+        actual_0 = _fw(self.input_w, *self.args_w[0])
         self.assertEqual(
             expected_0,
             actual_0,
             msg='Expected {}, but found {}.'.format(expected_0, actual_0)
         )
         expected_1 = 13063724.84
-        actual_1 = self.imc1._fw(self.input_w, *self.args_w[1])
+        actual_1 = _fw(self.input_w, *self.args_w[1])
         self.assertEqual(
             expected_1,
             actual_1,
             msg='Expected {}, but found {}.'.format(expected_1, actual_1)
         )
         expected_2 = 13063724.658222489
-        actual_2 = self.imc0_5._fw(self.input_w, *self.args_w[0.5])
+        actual_2 = _fw(self.input_w, *self.args_w[0.5])
         self.assertEqual(
             expected_2,
             actual_2,
@@ -200,7 +199,7 @@ class IMCTest(unittest.TestCase):
             [809800.01, 1094120.02, 1378440.03, 2050600.04, 2758664.05,
              3466728.06]
         )
-        actual_0 = self.imc0._fw_prime(self.input_w, *self.args_w[0])
+        actual_0 = _fw_prime(self.input_w, *self.args_w[0])
         np.testing.assert_array_equal(
             expected_0,
             actual_0,
@@ -210,7 +209,7 @@ class IMCTest(unittest.TestCase):
             [809800.01, 1094120.01, 1378440.01, 2050600.01, 2758664.01,
              3466728.01]
         )
-        actual_1 = self.imc1._fw_prime(self.input_w, *self.args_w[1])
+        actual_1 = _fw_prime(self.input_w, *self.args_w[1])
         np.testing.assert_array_equal(
             expected_1,
             actual_1,
@@ -220,7 +219,7 @@ class IMCTest(unittest.TestCase):
             [809800.01, 1094120.015, 1378440.02, 2050600.025, 2758664.03,
              3466728.035]
         )
-        actual_2 = self.imc0_5._fw_prime(self.input_w, *self.args_w[0.5])
+        actual_2 = _fw_prime(self.input_w, *self.args_w[0.5])
         np.testing.assert_allclose(
             expected_2,
             actual_2,
@@ -234,7 +233,7 @@ class IMCTest(unittest.TestCase):
              17628480.05]
         )
         hess_p = np.arange(6)
-        actual_0 = self.imc0._fw_hess(self.input_w, hess_p, *self.args_w[0])
+        actual_0 = _fw_hess(self.input_w, hess_p, *self.args_w[0])
         np.testing.assert_array_equal(
             expected_0,
             actual_0,
@@ -243,7 +242,7 @@ class IMCTest(unittest.TestCase):
         expected_1 = np.array(
             [4353520., 5755160., 7156800., 10723504., 14175992., 17628480.]
         )
-        actual_1 = self.imc1._fw_hess(self.input_w, hess_p, *self.args_w[1])
+        actual_1 = _fw_hess(self.input_w, hess_p, *self.args_w[1])
         np.testing.assert_array_equal(
             expected_1,
             actual_1,
@@ -253,8 +252,7 @@ class IMCTest(unittest.TestCase):
             [4353520., 5755160.005, 7156800.01, 10723504.015, 14175992.02,
              17628480.025]
         )
-        actual_2 = self.\
-            imc0_5._fw_hess(self.input_w, hess_p, *self.args_w[0.5])
+        actual_2 = _fw_hess(self.input_w, hess_p, *self.args_w[0.5])
         np.testing.assert_array_equal(
             expected_2,
             actual_2,
