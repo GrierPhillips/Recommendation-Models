7754cad37510c27964b2808d6230f640fbaf3cd8
Grier Phillips <GrierPhillips@gmail.com>
2017-08-25 09:41:35 -0600

    Reconfigure _fw tests based on restructuring of planned code.
    The new model for solving the IMC will utlize a concurrent approach to solving W and H simultaneously.
    This means that we can combine the _fw and _fh methods into one when solving for both.

--- tests/test_imc.py
+++ tests/test_imc.py
@@ -7,11 +7,12 @@ import unittest
 import warnings
 
 import numpy as np
+from scipy.sparse import lil_matrix
 from sklearn.exceptions import ConvergenceWarning
 
 from src.imc import (IMC, _fh, _fh_hess, _fh_prime,
-                     _fit_inductive_matrix_completion, _fw, _fw_hess,
-                     _fw_prime)
+                     _fit_inductive_matrix_completion, _cost, _cost_hess,
+                     _cost_prime)
 
 
 class IMCTest(unittest.TestCase):
@@ -38,173 +39,44 @@ class IMCTest(unittest.TestCase):
         X = np.array([[1, 2, 3], [4, 5, 6]])
         Y = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
         W = np.array([[1, 2, 3], [4, 5, 6]])
-        ratings = np.array([[300, 6000], [9000, 12000]])
-        self.data = {'R': ratings, 'X': X, 'Y': Y}
+        R = np.array([[300, 6000], [9000, 12000]])
+        rows, cols = lil_matrix(R).nonzero()
+        xh = X[rows]
+        yh = Y[rows]
+        bh = R[rows, cols]
+        self.data = {'R': R, 'X': X, 'Y': Y}
         args_h = {
-            key: (W, X, Y, ratings, 0.01, l1, H.shape)
+            key: (W, xh, yh, bh, 0.01, l1, H.shape)
             for (key, l1) in zip([0, 1, 0.5], [0, 1, 0.5])
         }
-        args_w = {
-            key: (H, X, Y, ratings, 0.01, l1, W.shape)
+        args_cost = {
+            key: (xh, yh, bh, 0.01, l1, H.size, H.shape, W.shape)
             for (key, l1) in zip([0, 1, 0.5], [0, 1, 0.5])
         }
-        self.args = {'args_h': args_h, 'args_w': args_w}
+        self.args = {'args_h': args_h, 'args_cost': args_cost}
+        self.input_cost = np.concatenate([H.flatten(), W.flatten()])
         self.input_h = H.flatten()
-        self.input_w = W.flatten()
 
     def tearDown(self):
         """Teardown the IMC class after each test."""
         pass
 
-    def test_fit(self):
-        """The fit method should call the fit_transform method and return self."""  # noqa
-        expected = True
-        result = self.imcs['imc0']\
-            .fit(self.data['R'], self.data['X'], self.data['X'])
-        actual = hasattr(result, 'components_')
-        self.assertEqual(
-            expected, actual,
-            msg='Expecte {}, but found {}.'.format(expected, actual)
-        )
-
-    def test_fit_transform(self):
-        """The fit_transform method should call the _fit_inductive_matrix_completion method to fit the IMC, finally returning the W matrix and returning a warning if convergence is not achieved."""  # noqa
-        expected_0 = np.array(
-            [[-0.833617, -0.15118367, 0.53124965],
-             [-0.37204305, -0.55720448, -0.74236591],
-             [0.40824829, -0.81649658, 0.40824829]]
-        )
-        actual_0 = self.imcs['imc0']\
-            .fit_transform(self.data['R'], self.data['X'], self.data['X'])
-        np.testing.assert_allclose(
-            expected_0, actual_0,
-            err_msg='Expected {}, but found {}.'.format(expected_0, actual_0)
-        )
-        expected_1 = np.array(
-            [[-0.83600716, -0.15478636, 0.52643444],
-             [-0.36664065, -0.55621445, -0.74578825]]
-        )
-        actual_1 = self.imcs['imck']\
-            .fit_transform(self.data['R'], self.data['X'], self.data['X'])
-        np.testing.assert_allclose(
-            expected_1, actual_1,
-            err_msg='Expected {}, but found {}.'.format(expected_1, actual_1),
-        )
-        with warnings.catch_warnings(record=True) as warn:
-            warnings.simplefilter('always')
-            self.imcs['imcw']\
-                .fit_transform(self.data['R'], self.data['X'], self.data['X'])
-            self.assertEqual(warn[0].category, ConvergenceWarning)
-
-    def test_fit_imc(self):
-        """The _fit_inductive_matrix_completion method should iterate through the AltMin-LRROM algorithm until convergence or max iterations is met, while printing messages in verbose mode."""  # noqa
-        expected_msg = 'Loss: 50639'
-        _fit_inductive_matrix_completion(
-            self.data['R'], self.data['X'], self.data['X'], W=None, H=None,
-            n_components=None, max_iter=1, verbose=1)
-        actual_msg = sys.stdout.getvalue().strip().split('.')[0]
-        self.assertEqual(
-            expected_msg, actual_msg,
-            msg='Expected {}, but found {}.'.format(expected_msg, actual_msg))
-
-    def test_fh(self):
-        """The _fh function should return the regularized sum squared error."""
-        expected_0 = 13063724.476444978
-        actual_0 = _fh(self.input_h, *self.args['args_h'][0])
+    def test_cost(self):
+        """The _cost function should return the regularized sum squared error."""
+        expected_0 = 37011644.476444982
+        actual_0 = _cost(self.input_cost, *self.args['args_cost'][0])
         self.assertEqual(
             expected_0, actual_0,
             msg='Expected {}, but found {}.'.format(expected_0, actual_0)
         )
-        expected_1 = 13063724.84
-        actual_1 = _fh(self.input_h, *self.args['args_h'][1])
+        expected_1 = 37011644.840000004
+        actual_1 = _cost(self.input_cost, *self.args['args_cost'][1])
         self.assertEqual(
             expected_1, actual_1,
             msg='Expected {}, but found {}.'.format(expected_1, actual_1)
         )
-        expected_2 = 13063724.658222489
-        actual_2 = _fh(self.input_h, *self.args['args_h'][0.5])
-        self.assertEqual(
-            expected_2, actual_2,
-            msg='Expected {}, but found {}.'.format(expected_2, actual_2)
-        )
-
-    def test_fh_prime(self):
-        """The _fh_prime function should return the gradient of the regularized sum squared error with respect to V."""  # noqa
-        expected_0 = np.array(
-            [568680.01, 641008.02, 713336.03, 785664.04, 1359780.05,
-             1528912.06, 1698044.07, 1867176.08]
-        )
-        actual_0 = _fh_prime(self.input_h, *self.args['args_h'][0])
-        np.testing.assert_array_equal(
-            expected_0, actual_0,
-            err_msg='Expected {}, but found {}.'.format(expected_0, actual_0)
-        )
-        expected_1 = np.array(
-            [568680.005, 641008.005, 713336.005, 785664.005, 1359780.005,
-             1528912.005, 1698044.005, 1867176.005]
-        )
-        actual_1 = _fh_prime(self.input_h, *self.args['args_h'][1])
-        np.testing.assert_array_equal(
-            expected_1, actual_1,
-            err_msg='Expected {}, but found {}.'.format(expected_1, actual_1)
-        )
-        expected_2 = np.array(
-            [568680.0075, 641008.0125, 713336.0175, 785664.0225, 1359780.0275,
-             1528912.0325, 1698044.0375, 1867176.0425]
-        )
-        actual_2 = _fh_prime(self.input_h, *self.args['args_h'][0.5])
-        np.testing.assert_allclose(
-            expected_2, actual_2,
-            err_msg='Expected {}, but found {}.'.format(expected_2, actual_2)
-        )
-
-    def test_fh_hess(self):
-        """The _fh_hess function should return the hessian of the regularized sum squared error with respect to V."""  # noqa
-        expected_0 = np.array(
-            [2622400., 3306176.01, 3989952.02, 4673728.03, 6261280.04,
-             7893872.05, 9526464.06, 11159056.07]
-        )
-        hess_p = np.arange(8)
-        actual_0 = _fh_hess(self.input_h, hess_p, *self.args['args_h'][0])
-        np.testing.assert_array_equal(
-            expected_0, actual_0,
-            err_msg='Expected {}, but found {}.'.format(expected_0, actual_0)
-        )
-        expected_1 = np.array(
-            [2622400., 3306176., 3989952., 4673728., 6261280.,
-             7893872., 9526464., 11159056.]
-        )
-        actual_1 = _fh_hess(self.input_h, hess_p, *self.args['args_h'][1])
-        np.testing.assert_array_equal(
-            expected_1, actual_1,
-            err_msg='Expected {}, but found {}.'.format(expected_1, actual_1)
-        )
-        expected_2 = np.array(
-            [2622400., 3306176.005, 3989952.01, 4673728.015, 6261280.02,
-             7893872.025, 9526464.03, 11159056.035]
-        )
-        actual_2 = _fh_hess(self.input_h, hess_p, *self.args['args_h'][0.5])
-        np.testing.assert_array_equal(
-            expected_2, actual_2,
-            err_msg='Expected {}, but found {}.'.format(expected_2, actual_2)
-        )
-
-    def test_fw(self):
-        """The _fw function should return the regularized sum squared error."""
-        expected_0 = 13063724.476444978
-        actual_0 = _fw(self.input_w, *self.args['args_w'][0])
-        self.assertEqual(
-            expected_0, actual_0,
-            msg='Expected {}, but found {}.'.format(expected_0, actual_0)
-        )
-        expected_1 = 13063724.84
-        actual_1 = _fw(self.input_w, *self.args['args_w'][1])
-        self.assertEqual(
-            expected_1, actual_1,
-            msg='Expected {}, but found {}.'.format(expected_1, actual_1)
-        )
-        expected_2 = 13063724.658222489
-        actual_2 = _fw(self.input_w, *self.args['args_w'][0.5])
+        expected_2 = 37011644.658222489
+        actual_2 = _cost(self.input_cost, *self.args['args_cost'][0.5])
         self.assertEqual(
             expected_2, actual_2,
             msg='Expected {}, but found {}.'.format(expected_2, actual_2)
